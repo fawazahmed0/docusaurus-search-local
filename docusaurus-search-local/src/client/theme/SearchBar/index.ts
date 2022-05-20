01ef4d78d8bf434a9b14d7a74164763a8a1357c1
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-20 20:56:16 +0000

    revert things

--- docusaurus-search-local/src/client/theme/SearchBar/index.ts
+++ docusaurus-search-local/src/client/theme/SearchBar/index.ts
@@ -1,9 +1,3 @@
 import "../../utils/proxiedGenerated";
 import SearchBar from "./SearchBar";
-import { fetchIndexes } from "./fetchIndexes";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-const {
-    siteConfig: { baseUrl },
-  } = useDocusaurusContext();
-fetchIndexes(baseUrl);
 export default SearchBar;
